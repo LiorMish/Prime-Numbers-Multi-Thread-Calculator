3a4d75953c95ccb715a113abf740fece95da5251
LiorMish <85901184+LiorMish@users.noreply.github.com>
2022-05-26 12:09:17 +0300

    Add files via upload

---
+++ PrimeCalc/PrimeCalc/PrimesCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCalc
+{
+    public class PrimeCalculator
+    {
+        public PrimeCalculator()
+        {
+        }
+
+
+        /* The function below will check if the number is a prime number */
+        private bool isPrime(long n)
+        {
+            if (n == 2 || n == 3)
+                return true;
+
+            if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+                return false;
+            for (int i = 5; i <= Math.Sqrt(n); i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        /* The function below will find and print all the prime numbers between the two numbers */
+        public void findPrimes(long firstNum, long secondNum)
+        {
+            while (firstNum < secondNum)
+            {
+                if (isPrime(firstNum))
+                {
+                    Console.WriteLine("Thread [" + Thread.CurrentThread.ManagedThreadId + "]: " + firstNum);
+
+                }
+
+                firstNum++;
+            }
+        }
+    }
+}
---
+++ PrimeCalc/PrimeCalc/Program.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using PrimeCalc;
+
+class MainClass
+{
+    /* the main function below will find all the prime numbers between two numbers given as arguments, using threads that also given as argument */
+    static void Main(string[] args)
+    {
+        // Check there is enough arguments
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Wrong number of arguments");
+            return;
+        }
+        else
+        {
+            long firstNum = long.Parse(args[0]);
+            long lastNum = long.Parse(args[1]);
+            int numOfThreads = (int)long.Parse(args[2]);
+            long range = (lastNum - firstNum) / numOfThreads;
+
+            // Check the second number greater then the first.
+            if (firstNum >= lastNum)
+            {
+                Console.WriteLine("The second number has to be bigger then the first.");
+                return;
+            }
+            // Check the numbers are positive.
+            if (firstNum < 0 || lastNum < 0)
+            {
+                Console.WriteLine("The numbers has to be positive.");
+                return;
+            }
+            // Check the amount of threads is positive.
+            if (numOfThreads <= 0)
+            {
+                Console.WriteLine("The number of threads has to be positive.");
+                return;
+            }
+
+            PrimeCalculator primeCalculator = new PrimeCalculator();
+            
+            Thread[] threads = new Thread[numOfThreads];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                long tempNum = firstNum;
+                // Give the last thread the rest of the numbers 
+                if (i == numOfThreads - 1)
+                {
+                    threads[i] = new Thread(() => primeCalculator.findPrimes(tempNum, lastNum));
+                    threads[i].Start();
+                }
+                // Divide the work by the range of the numbers
+                else
+                {
+                    threads[i] = new Thread(() => primeCalculator.findPrimes(tempNum, tempNum + range));
+                    threads[i].Start();
+                    firstNum += range;
+
+                }
+
+            }
+
+            // Waiting for all threads to complete
+            foreach (Thread thread in threads)
+            { thread.Join(); }
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
